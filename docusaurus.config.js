812a38e67c8f46b63b93479b414b8c9470ca7886
FxRayHughes <bushuaichen@yeah.net>
2023-01-27 14:45:00 +0800

    ++

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,6 +1,7 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
+
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
@@ -105,6 +106,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+        additionalLanguages: ['powershell','java','kotlin'],
       },
     }),
 };
