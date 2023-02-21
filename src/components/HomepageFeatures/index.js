e8b091fd5f4b0ca23754dc7f805db01edbfbe029
FxRayHughes <bushuaichen@yeah.net>
2023-02-21 09:58:40 +0800

    ++

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -3,33 +3,33 @@ import clsx from 'clsx';
 import styles from './styles.module.css';
 
 const FeatureList = [
-  {
-    title: '便捷使用',
-    Svg: require('@site/static/img/undraw_experience_design_re_dmqq.svg').default,
-    description: (
-      <>
-        这用起来和常规的SkillAPI没有什么不同
-      </>
-    ),
-  },
-  {
-    title: '永久免费',
-    Svg: require('@site/static/img/free.svg').default,
-    description: (
-      <>
-        是的我并不需要FxSkillAPI来进行盈利，当然如果这个插件帮到你了，你也可以给我一些赞助~
-      </>
-    ),
-  },
-  {
-    title: '极快的反馈',
-    Svg: require('@site/static/img/undraw_contact_us_re_4qqt.svg').default,
-    description: (
-      <>
-        经过半年用户的验证，出现BUG/问题反馈，我都会在24*2h内回复并**解决**，给你一个更好的使用环境
-      </>
-    ),
-  },
+  // {
+  //   title: '便捷使用',
+  //   Svg: require('@site/static/img/undraw_experience_design_re_dmqq.svg').default,
+  //   description: (
+  //     <>
+  //       这用起来和常规的SkillAPI没有什么不同
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: '永久免费',
+  //   Svg: require('@site/static/img/free.svg').default,
+  //   description: (
+  //     <>
+  //       是的我并不需要FxSkillAPI来进行盈利，当然如果这个插件帮到你了，你也可以给我一些赞助~
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: '极快的反馈',
+  //   Svg: require('@site/static/img/undraw_contact_us_re_4qqt.svg').default,
+  //   description: (
+  //     <>
+  //       经过半年用户的验证，出现BUG/问题反馈，我都会在24*2h内回复并**解决**，给你一个更好的使用环境
+  //     </>
+  //   ),
+  // },
 ];
 
 function Feature({Svg, title, description}) {
